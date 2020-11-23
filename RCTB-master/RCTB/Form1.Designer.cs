e49a7c72af0942881dede7fb1ac9595f2ff4d737
xenox187 <74907172+xenox187@users.noreply.github.com>
2020-11-23 12:19:28 +0100

    Add files via upload

---
+++ RCTB-master/RCTB/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace RCTB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(136, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Triggerbot";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(348, 107);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rogue Company Trigger Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ RCTB-master/RCTB/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+
+namespace RCTB
+{
+    public partial class Form1 : Form
+    {
+        //  ez ez = new ez();
+
+        AutoItX3 au3 = new AutoItX3();
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        object pix;
+        int newCol;
+        int[] ScreenCoor = { Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2 };
+        int x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            //   ez.SetInvi(this);
+            //   ez.StartLoop(10, "Rogue Company  ", this);
+            Thread tb = new Thread(Trigger) { IsBackground = true };
+            tb.Start();
+        }
+        private void Trigger()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.F1) < 0)
+                {
+                    checkBox1.ForeColor = Color.Green;
+                    checkBox1.Checked = true;
+                }
+                if (GetAsyncKeyState(Keys.F2) < 0)
+                {
+                    checkBox1.ForeColor = Color.Red;
+                    checkBox1.Checked = false;
+                }
+                if (checkBox1.Checked)
+                {
+
+
+                    if (GetAsyncKeyState(Keys.RButton) < 0)
+                    {
+                        newCol = GrabColor();
+                        textBox1.Text = newCol.ToString();
+                        if (newCol > 16551953)
+                        {
+                            Thread.Sleep(1);
+                            au3.MouseClick("LEFT");
+                        }
+                    }
+
+                    if (GetAsyncKeyState(Keys.F10) < 0)
+                    {
+                        this.Close();
+                    }
+
+                }
+
+                Thread.Sleep(1);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int GrabColor()
+        {
+            pix = au3.PixelGetColor(ScreenCoor[0], ScreenCoor[1]);
+            return Int32.Parse(pix.ToString());
+
+        }
+    }
+}
